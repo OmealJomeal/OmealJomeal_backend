cc1f766620f9b1ca0f0734ab664554624b6878c5
ijeon-yeong <qljy22@gmail.com>
2022-08-22 10:14:27 +0900

    fix :datasource

--- src/main/java/com/omealjomeal/OmealJomealApplication.java
+++ src/main/java/com/omealjomeal/OmealJomealApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = "classpath:datasource.properties")
 public class OmealJomealApplication {
 
     public static void main(String[] args) {
